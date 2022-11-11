adaf349b83816ed622c0cc0d85f47eb82fce702c
JFmaia <jfmaia741@gmail.com>
2022-11-11 15:30:26 -0300

    chore: Passando atributos via props.

--- src/components/TaskGrid.vue
+++ src/components/TaskGrid.vue
@@ -1,19 +1,22 @@
 <template>
-    <div class="tasks">
-        <template>
-            <div v-for="task in tasks" :key="task.name">{{task.name}}</div>
+    <div class="task-grid">
+        <template v-if="tasks.length">
+            <Task v-for="task in tasks" :key="task.name" :task="task"></Task>
         </template>
+        <p v-else class="no-task">Sua vida está em <strong>dia!</strong></p>
     </div>
 </template>
 
 <script>
+import Task from "./Task.vue"
 export default {
+    components:{Task},
     props:{
         tasks: {type:Array, required: true}
     }
 }
 </script>
 
-<style>
+<style scoped>
 
 </style>
