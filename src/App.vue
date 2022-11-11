3deb992a3308966750b98cbf222261a0bae00ce7
JFmaia <jfmaia741@gmail.com>
2022-11-11 09:57:36 -0300

    Commit inicial, com a base!

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+	<div id="app">
+		<h1>Tarefas</h1>
+	</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+	body {
+		font-family: 'Lato', sans-serif;
+		background: #15141B;
+		color: #A277FF;
+	}
+
+	#app {
+		display: flex;
+		flex: 1;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		height: 100vh;
+	}
+
+	#app h1 {
+		margin-bottom: 5px;
+		font-weight: 300;
+		font-size: 3rem;
+	}
+</style>
