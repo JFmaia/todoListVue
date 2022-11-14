181687989cea4e1002937f61efbfb0d47b3026dc
JFmaia <jfmaia741@gmail.com>
2022-11-14 10:22:10 -0300

    chore: Utilizando o deep para o watch observar até a mudança de estado e salvar no localStirage.

--- src/App.vue
+++ src/App.vue
@@ -36,6 +36,15 @@ export default {
 			return Math.round(done/total*100) || 0
 		}
 	},
+	watch:{
+		// Deep faz o watch observar profunda até as mudanças de estado
+		tasks:{
+			deep: true,
+			handler(){
+				localStorage.setItem('tasks', JSON.stringify(this.tasks))
+			}
+		}
+	},
 	methods:{
 		addTask(task){
 			const sameName = t => t.name === task.name
@@ -54,6 +63,11 @@ export default {
 		toogleTaksState(i){
 			this.tasks[i].pending = !this.tasks[i].pending
 		}
+	},
+	created(){
+		const json = localStorage.getItem('tasks')
+		const array = JSON.parse(json)
+		this.tasks = Array.isArray(array) ? array : []
 	}
 }
 </script>
