1e343baab50ca70f29545895b611cf84f6e1f889
JFmaia <jfmaia741@gmail.com>
2022-11-14 08:32:37 -0300

    chore: Adicionado estilo no componente.

--- src/components/NewTask.vue
+++ src/components/NewTask.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="new-task">
-    <input v-model="name" @keydown.enter="add()" type="text">
-    <button @click="add()">+</button>
+    <input 
+        v-model="name" 
+        @keydown.enter="add()" 
+        type="text"
+        class="form-element"
+        placeholder="Nova tarefa?"
+    >
+    <button class="form-element" @click="add()">+</button>
   </div>
 </template>
 
@@ -21,6 +27,25 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+    .form-element{
+        outline: none;
+        font-size: 2rem;
+        border: 2px solid #7437f7;
+        padding: 5px 10px 8px;
+        color: #EDECEE;
+    }
+    input.form-element{
+        width: 500px;
+        background: transparent;
+        border-bottom-left-radius: 8px;
+        border-top-left-radius: 8px;
+    }
+    button.form-element{
+        border-left: none;
+        background: #7437f7;
+        color: #EDECEE;
+        border-bottom-right-radius: 8px;
+        border-top-right-radius: 8px;  
+    }
 </style>
