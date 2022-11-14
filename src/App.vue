330197ec8e394a2db713acaed6510e8ea50fe74b
JFmaia <jfmaia741@gmail.com>
2022-11-14 08:18:59 -0300

    chore: Adicionando o componente e recebendo o evento no pai.

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,39 @@
 <template>
 	<div id="app">
 		<h1>Tarefas</h1>
+	<!-- Por padrão ao emitir um evento para o pai o valor já passa para a função -->
+		<NewTask @taskAdded="addTask">
+
+		</NewTask>
 		<TaskGrid :tasks="tasks"/>
 	</div>
 </template>
 
 <script>
 import TaskGrid from "./components/TaskGrid.vue"
+import NewTask from "./components/NewTask.vue";
+
 export default {
-	components:{TaskGrid},
+	components:{TaskGrid, NewTask},
 	data(){
 		return{
 			tasks:[
 				{name:"Lavar louça", pending: false},
 				{name:"Estudar 2hr pela manhã!", pending: true},
 			]
+		}
+	},
+	methods:{
+		addTask(task){
+			const sameName = t => t.name === task.name
+			// Se o filter no final for 0 não os nomes não são iquais, se não, não poderá ser salvo a nova task
+			const reallyNew = this.tasks.filter(sameName).length == 0
+			if(reallyNew){
+				this.tasks.push({
+					name:task.name,
+					pending:task.pending || true
+				})
+			}
 		}
 	}
 }
