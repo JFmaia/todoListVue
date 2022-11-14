4533ad8bf72932adb09daa267635d2c72b9caf20
JFmaia <jfmaia741@gmail.com>
2022-11-14 10:02:58 -0300

    Adicionando componente novo e passando valor de progresso
    Ajustando o layout pra ficar ajustavel ao tamanho da lista de tarefas.

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,39 @@
 <template>
-	<div id="app">
-		<h1>Tarefas</h1>
-	<!-- Por padrão ao emitir um evento para o pai o valor já passa para a função -->
-		<NewTask @taskAdded="addTask">
+	<div class="container-todo">
+		<div id="app" class="container">
+			<h1>Tarefas</h1>
+			<TaskProgress :progress="progress"/>
+			<!-- Por padrão ao emitir um evento para o pai o valor já passa para a função -->
+			<NewTask @taskAdded="addTask">
 
-		</NewTask>
-		<TaskGrid 
-			:tasks="tasks" 
-			@taskDeleted="deleteTask"
-			@taskStateChanged="toogleTaksState"
-		/>
+			</NewTask>
+			<TaskGrid 
+				:tasks="tasks" 
+				@taskDeleted="deleteTask"
+				@taskStateChanged="toogleTaksState"
+			/>
+		</div>
 	</div>
+	
 </template>
 
 <script>
 import TaskGrid from "./components/TaskGrid.vue"
 import NewTask from "./components/NewTask.vue";
+import TaskProgress from "./components/TaskProgress.vue";
 
 export default {
-	components:{TaskGrid, NewTask},
+	components:{TaskGrid, NewTask, TaskProgress},
 	data(){
 		return{
 			tasks:[]
+		}
+	},
+	computed:{
+		progress(){
+			const total = this.tasks.length
+			const done = this.tasks.filter(t=> !t.pending).length
+			return Math.round(done/total*100) || 0
 		}
 	},
 	methods:{
@@ -48,10 +60,20 @@ export default {
 
 <style>
 	/* EDECEE color de textos normais */
-	body {
+	html,body{
+		height: 100%;
 		font-family: 'Lato', sans-serif;
 		background: #15141B;
 		color: #7437f7;
+	}
+
+	.container-todo{
+		padding: 48px 78px;
+	}
+
+	.container{
+		max-width: 1444px;
+		margin: 0 auto;
 	}
 
 	#app {
@@ -60,7 +82,7 @@ export default {
 		flex-direction: column;
 		justify-content: center;
 		align-items: center;
-		height: 100vh;
+		min-height: 100%;
 		gap: 100px;
 	}
 
